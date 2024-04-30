f1a682750b435c595d286708d747eaaa41c16cbe
apostasie <spam_blackhole@farcloser.world>
2024-04-30 14:08:29 -0700

    Cleanup

--- dedup.sh
+++ dedup.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
 
+# Bash 5 only
 declare -A md5list
 declare -A filelist
 
-md5list=()
-filelist=()
-while read -r file; do
-  dsum="$(md5sum "$file")"
-  dsum="${dsum%% *}"
-  filelist["$file"]="$dsum"
-  md5list["$dsum"]+="*"
-done < <(find . -type f -not -iname "*@eaDir*")
+far::dedup(){
+  md5list=()
+  filelist=()
+  while read -r file; do
+    dsum="$(md5sum "$file")"
+    dsum="${dsum%% *}"
+    filelist["$file"]="$dsum"
+    md5list["$dsum"]+="*"
+  done < <(find "$1" -type f)
 
-
-for i in "${!md5list[@]}"
-do
-  if [ "${md5list[$i]}" != "" ] && [ "${md5list[$i]}" != "*" ] ; then
-    echo "key  : $i"
-    echo "value: ${md5list[$i]}"
-    for j  in "${!filelist[@]}"; do
-      if [ "${filelist[$j]}" == "$i" ]; then
-        if [ "$(file --brief --mime-type "$j")" != "text/plain" ]; then
-          echo "guilty: $j"
+  for i in "${!md5list[@]}"
+  do
+    if [ "${md5list[$i]}" != "" ] && [ "${md5list[$i]}" != "*" ] ; then
+      echo "Duplicate:"
+      echo "- key  : $i"
+      echo "- value: ${md5list[$i]}"
+      for j  in "${!filelist[@]}"; do
+        if [ "${filelist[$j]}" == "$i" ]; then
+          if [ "$(file --brief --mime-type "$j")" != "text/plain" ]; then
+            echo "Duplicate entries: $j"
+          fi
         fi
-      fi
-    done
-  fi
-done
+      done
+    fi
+  done
+}
+
+far::dedup "${1:-$(pwd)}"
